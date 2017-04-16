1b3d470e5f65d1fa2fbb57798b6ef6357c08c42d
HaroldRKingsberg <haroldrkingsberg@gmail.com>
2017-04-15 23:58:02 -0400

    bugfix - time incorporated into non-stochastic terms

--- stock.py
+++ stock.py
@@ -9,16 +9,17 @@ class Stock(object):
         self.spot = spot
 
     @abc.abstractmethod
-    def find_volatilities(self, steps, *args):
+    def find_volatilities(self, time_step, vol_steps):
         ''' Return the volatility values over time '''
 
-    def walk_price(self, risk_free, price_steps, vol_steps=None):
+    def walk_price(self, risk_free, time_step, price_steps, vol_steps=None):
         vol_steps = price_steps if vol_steps is None else vol_steps
+        vols = self.find_volatilities(time_step, vol_steps):
         price = self.spot
         yield price
 
-        for pstep, vol in itertools.izip(price_steps, self.find_volatilities(vol_steps)):
-            det_term = risk_free * price
+        for pstep, vol in itertools.izip(price_steps, vols):
+            det_term = risk_free * price * time_step
             sto_term = (vol**0.5) * pstep * price
             price += (det_term + sto_term)
             yield price
@@ -30,8 +31,8 @@ class ConstantVolatilityStock(Stock):
         super(ConstantVolatilityStock, self).__init__(spot)
         self._vol = _vol
 
-    def find_volatilities(self, steps, *args):
-        return (self._vol for _ in steps)
+    def find_volatilities(self, time_step, vol_steps):
+        return (self._vol for _ in vol_steps)
 
 
 class VariableVolatilityStock(Stock):
@@ -43,12 +44,12 @@ class VariableVolatilityStock(Stock):
         self._k = k
         self._g = g
 
-    def find_volatilities(self, steps, *args):
+    def find_volatilities(self, time_step, vol_steps):
         vol = self._base_vol
         yield vol
 
-        for s in steps:
-            det_term = self._k * (self._base_vol - vol)
+        for s in vol_steps:
+            det_term = self._k * (self._base_vol - vol) * time_step
             sto_term = self._g * (vol**0.5) * s
             vol += (det_term + sto_term)
             yield vol
