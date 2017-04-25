aee0fb3d09ff363d8cdf0b03c0423936669e8a86
Peter Will <peter.will@capitalone.com>
2017-04-25 16:10:05 -0400

    added setup.py file, and moved src and test dirs into package directory.  app.py import mlmc package and should be main executable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='mlmc',
+    version='.1',
+    author='HMP',
+    url='https://github.com/HaroldRKingsberg/MLMC',
+    packages=['mlmc'],
+    install_requires=['numpy']
+)
