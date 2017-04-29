de71d6ec951881b5af046c58bb03da238fc666c8
Peter Will <peter.will@capitalone.com>
2017-04-29 18:50:26 -0400

    fixed math.max to max for python==2.7.9

--- tests/test_path.py
+++ tests/test_path.py
@@ -2,9 +2,9 @@ import unittest
 import numpy as np
 import math
 
-from mlmc.src.path import create_simple_path, create_layer_path
-from mlmc.src.stock import ConstantVolatilityStock, VariableVolatilityStock
-from mlmc.src.random_numbers import SampleCreator, IIDSampleCreator, CorrelatedSampleCreator
+from mlmc.path import create_simple_path, create_layer_path
+from mlmc.stock import ConstantVolatilityStock, VariableVolatilityStock
+from mlmc.random_numbers import SampleCreator, IIDSampleCreator, CorrelatedSampleCreator
 
 class SimplePathTestCase(unittest.TestCase):
     '''
@@ -18,7 +18,7 @@ class SimplePathTestCase(unittest.TestCase):
         def __init__(self, size=4):
             # need a no arg constructor
             super(ConstRng, self).__init__(size)
-        
+
         def create_sample(self, n_samples=1, time_step=1, *args):
             x = np.array([1 for _ in xrange(n_samples)])
             return np.array([x for _ in xrange(self.size)])
@@ -36,15 +36,15 @@ class SimplePathTestCase(unittest.TestCase):
         base_vol, kappa, theta, gamma, r = 0.8, 0.5, 1.2, 1, 0.01
         stock_price = range(1,3)
         stocks = [VariableVolatilityStock(spot, base_vol, kappa, theta, gamma) for spot in stock_price]
-        
+
         T, n_steps = 10, 252000
         dt = float(T) / n_steps
         var = base_vol**2
         dW, dZ = 1, 1 # because we are testing a rng that returns 1 only
         for i in xrange(n_steps):
-            var += kappa * (theta - var) * dt + gamma * math.max(var,0)**0.5 * dZ
+            var += kappa * (theta - var) * dt + gamma * max(var,0)**0.5 * dZ
             for j in xrange(len(stock_price)):
-                vol = math.max(var,0)**0.5
+                vol = max(var,0)**0.5
                 stock_price[j] = stock_price[j] * math.exp((r - 0.5 * vol**2) * dt + vol * dW)
         post_walk_price = create_simple_path(stocks, r, T, n_steps, ConstOneRng)
         for j in xrange(len(stock_price)):
