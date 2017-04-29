f6ac474a023bcf56c934d8c8944da0a4df92d019
mtran2011 <tranhaminh@gmail.com>
2017-04-29 00:22:08 -0400

    test create_simple_path

--- mlmc/src/path.py
+++ mlmc/src/path.py
@@ -10,8 +10,10 @@ def create_simple_path(stocks,
                        rng_creator=None,
                        chunk_size=100000):
     '''
-    Make each of the inputted stocks walk one path to final time t
-    Each path consists of n_steps. 
+    Get the post walk price of each of the inputted stocks
+    Each stock walks one path to final time T
+    The post walk price is returned without changing the stock itself
+    This path simulation is vanilla Monte Carlo for Euler Maruyama
     To address memory issues, the simulation is run in chunks.
     Args:
         stocks (iterable): list of stocks that will walk
@@ -22,13 +24,17 @@ def create_simple_path(stocks,
         chunk_size (long): a walk of n_steps is done in chunks to address 
             memory issues; chunk_size is the size of one chunk
     Returns:
+        list: the post walk price of each stock
     '''
-                    
+
+    # we do not change the stock itself 
     stocks = [copy.deepcopy(s) for s in stocks]
 
     # rng is a random number generator
-    # if the user provides a maker of rng, then use that
-    # if the maker rng_creator is not given, then default 
+    # if the user provides a maker of rng, then use that; if not default
+    # rng_creator is a class with a no arg __init__
+    # for each stock, rng is making one series for dW and one for dZ
+    # hence rng.size = 2 * num of stocks 
     rng = rng_creator() if rng_creator else random.IIDSampleCreator(2*len(stocks))
 
     dt = float(T) / n_steps
@@ -47,10 +53,15 @@ def create_simple_path(stocks,
         # rng.size is like how many stocks we are simulating
         # num of rows of samples = rng.size, num columns = c
         samples = rng.create_sample(n_samples=c, time_step=dt)
-        # len(samples) := num rows; 
+        # len(samples) := num rows = rng.size
+        # in defautl case interval = 2 
         interval = len(samples) / len(stocks)
 
         for i, s in enumerate(stocks):
+            # each stock s will use 1 row of len c for dW and 1 for dZ;
+            # dZ is driving the stochastic vol over each step
+            # samples[i*interval] are the dW
+            # samples[(i+1)*interval] are the dZ
             s.walk_price(risk_free,
                          dt,
                          *samples[i*interval:(i+1)*interval])
@@ -60,11 +71,29 @@ def create_simple_path(stocks,
 
 def create_layer_path(stocks,
                       risk_free,
-                      t,
+                      T,
                       n_steps,
                       rng_creator=None,
                       chunk_size=100000,
                       K=2):
+    '''
+    Get the post walk price of each of the inputted stocks
+    Each stock walks one path to final time T
+    The post walk price is returned without changing the stock itself
+    This path simulation is MultiLevel Monte Carlo for Euler Maruyama
+    To address memory issues, the simulation is run in chunks.
+    Args:
+        stocks (iterable): list of stocks that will walk
+        risk_free (float): risk free rate driving stock drift
+        T (float): the final time at end of a walk
+        n_steps (long): number of steps along one path
+        rng_creator (object): a maker of random number generator
+        chunk_size (long): a walk of n_steps is done in chunks to address 
+            memory issues; chunk_size is the size of one chunk
+    Returns:
+        list: the post walk price of each stock
+    '''
+
     stocks = [
         (copy.deepcopy(s), copy.deepcopy(s))
         for s in stocks
@@ -72,7 +101,7 @@ def create_layer_path(stocks,
 
     rng = rng_creator() if rng_creator else random.IIDSampleCreator(2*len(stocks))
 
-    dt = float(t) / n_steps
+    dt = float(T) / n_steps
     dt_sub = dt / K
     chunks = [chunk_size for _ in xrange(n_steps/chunk_size)]
     chunks.append(n_steps % chunk_size)
---
+++ mlmc/test/test_path.py
@@ -0,0 +1,54 @@
+import unittest
+import numpy as np
+import math
+
+from mlmc.src.path import create_simple_path, create_layer_path
+from mlmc.src.stock import ConstantVolatilityStock, VariableVolatilityStock
+from mlmc.src.random_numbers import SampleCreator, IIDSampleCreator, CorrelatedSampleCreator
+
+class SimplePathTestCase(unittest.TestCase):
+    '''
+    To test create_simple_path()
+    '''
+
+    class ConstOneRng(SampleCreator):
+        '''
+        A rng that returns constants of value = 1
+        '''
+        def __init__(self, size=4):
+            # need a no arg constructor
+            super(ConstRng, self).__init__(size)
+        
+        def create_sample(self, n_samples=1, time_step=1, *args):
+            x = np.array([1 for _ in xrange(n_samples)])
+            return np.array([x for _ in xrange(self.size)])
+
+    def test_stocks_input_unchanged(self):
+        # create some stocks, record their current post_walk_price
+        # run create_simple_path and confirm inputted stocks are not changed
+        stocks = [ConstantVolatilityStock(i,1) for i in xrange(1,5)]
+        current_price = [i for i in xrange(1,5)]
+        _ = create_simple_path(stocks, risk_free=0.01, T=10, n_steps=252000)
+        for j in xrange(len(stocks)):
+            self.assertEqual(stocks[j].post_walk_price, current_price[j])
+
+    def test_with_const_rng(self):
+        base_vol, kappa, theta, gamma, r = 0.8, 0.5, 1.2, 1, 0.01
+        stock_price = range(1,3)
+        stocks = [VariableVolatilityStock(spot, base_vol, kappa, theta, gamma) for spot in stock_price]
+        
+        T, n_steps = 10, 252000
+        dt = float(T) / n_steps
+        var = base_vol**2
+        dW, dZ = 1, 1 # because we are testing a rng that returns 1 only
+        for i in xrange(n_steps):
+            var += kappa * (theta - var) * dt + gamma * math.max(var,0)**0.5 * dZ
+            for j in xrange(len(stock_price)):
+                vol = math.max(var,0)**0.5
+                stock_price[j] = stock_price[j] * math.exp((r - 0.5 * vol**2) * dt + vol * dW)
+        post_walk_price = create_simple_path(stocks, r, T, n_steps, ConstOneRng)
+        for j in xrange(len(stock_price)):
+            self.assertAlmostEqual(stock_price[j], post_walk_price[j])
+
+if __name__ == '__main__':
+    unittest.main()
