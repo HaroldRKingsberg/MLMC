fc9337834bac02e64f71aa5411c20e99964d155d
mtran2011 <tranhaminh@gmail.com>
2017-04-29 20:39:26 -0400

    test simple path

--- tests/test_path.py
+++ tests/test_path.py
@@ -6,16 +6,16 @@ from mlmc.path import create_simple_path, create_layer_path
 from mlmc.stock import ConstantVolatilityStock, VariableVolatilityStock
 from mlmc.random_numbers import SampleCreator, IIDSampleCreator, CorrelatedSampleCreator
 
-class ConstOneRng(SampleCreator):
+class ConstRng(SampleCreator):
     '''
-    A rng that returns constants of value = 1
+    A rng that returns constants of value = 0.01
     '''
     def __init__(self, size=4):
         # need a no arg constructor
         super(ConstRng, self).__init__(size)
     
     def create_sample(self, n_samples=1, time_step=1, *args):
-        x = np.array([1 for _ in xrange(n_samples)])
+        x = np.array([0.01 for _ in xrange(n_samples)])
         return np.array([x for _ in xrange(self.size)])
 
 class SimplePathTestCase(unittest.TestCase):
@@ -28,7 +28,7 @@ class SimplePathTestCase(unittest.TestCase):
         # run create_simple_path and confirm inputted stocks are not changed
         stocks = [ConstantVolatilityStock(i,1) for i in xrange(1,5)]
         current_price = [i for i in xrange(1,5)]
-        _ = create_simple_path(stocks, risk_free=0.01, T=10, n_steps=252000)
+        _ = create_simple_path(stocks, risk_free=0.01, T=10, n_steps=202000)
         for j in xrange(len(stocks)):
             self.assertEqual(stocks[j].post_walk_price, current_price[j])
 
@@ -37,10 +37,10 @@ class SimplePathTestCase(unittest.TestCase):
         stock_price = range(1,3)
         stocks = [VariableVolatilityStock(spot, base_vol, kappa, theta, gamma) for spot in stock_price]
         
-        T, n_steps = 10, 252000
+        T, n_steps = 10, 202000
         dt = float(T) / n_steps
         var = base_vol**2
-        dW, dZ = 1, 1 # because we are testing a rng that returns 1 only
+        dW, dZ = 0.01, 0.01 # because we are testing a rng that returns 0.01
         
         # first manually walk the stocks and store results in stock_price
         for i in xrange(n_steps):
@@ -50,7 +50,7 @@ class SimplePathTestCase(unittest.TestCase):
                 stock_price[j] = stock_price[j] * math.exp((r - 0.5 * vol**2) * dt + vol * dW)
         
         # now use create_simple_path() to walk
-        post_walk_price = create_simple_path(stocks, r, T, n_steps, ConstOneRng)
+        post_walk_price = create_simple_path(stocks, r, T, n_steps, ConstRng)
 
         for j in xrange(len(stock_price)):
             self.assertAlmostEqual(stock_price[j], post_walk_price[j])
@@ -64,10 +64,10 @@ class LayerPathTestCase(unittest.TestCase):
         stock_price = range(1,3)
         stocks = [VariableVolatilityStock(spot, base_vol, kappa, theta, gamma) for spot in stock_price]
         
-        T, n_steps = 10, 252000
+        T, n_steps = 10, 202000
         dt = float(T) / n_steps
         var = base_vol**2
-        dW, dZ = 1, 1 # because we are testing a rng that returns 1 only
+        dW, dZ = 0.01, 0.01 # because we are testing a rng that returns 0.01
 
 if __name__ == '__main__':
     unittest.main()
