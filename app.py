e7c8d3767ed0da705b5c245e41eb6951aa8a56dd
Peter Will <peter.will@capitalone.com>
2017-04-30 16:18:19 -0400

    adding in minh methods to option class

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from mlmc import option, stock, random_numbers
 from tests.analytic import black_scholes
 import math
+
 spot = 100
 vol = .1
 risk_free = .05
 strike = 120
 expiry = 1
+eps=.05
 
-
-
-b = black_scholes(S0=spot, K=strike, r=risk_free, q=0, sigma=vol, T=expiry, option_type="call")
-
-for i in xrange(5):
-    s = stock.ConstantVolatilityStock(spot, vol)
-    e = option.EuropeanCall([s], risk_free, strike, expiry)
-    p = e.euler_asset_walk(20000, 5000)
-    exp_p = math.exp(-risk_free*expiry)*p.mean()
-    resid = abs(b-exp_p)
-    print(resid)
+s = stock.ConstantVolatilityStock(spot, vol)
+e = option.EuropeanCall(s, risk_free, strike, expiry)
+count = 0
+print(e.check_vanilla_mc(target_stdev=eps, confidence_level=.51, num_samples=100))
--- mlmc/option.py
+++ mlmc/option.py
@@ -1,7 +1,11 @@
+from __future__ import division
 from mlmc import path, stock
 import multiprocessing
 import pprint
 import numpy as np
+from scipy.stats import norm
+from tests.analytic import black_scholes
+import math
 
 class Option(object):
 
@@ -34,10 +38,10 @@ class EuropeanCall(Option):
         #     ]
         # )
 
-        x = np.zeros((npaths, len(self.assets)))
+        x = np.zeros((npaths, 1))
         for i in xrange(npaths):
             x[i,:] = path.create_simple_path(
-                self.assets,
+                [self.assets],
                 self.risk_free,
                 self.expirary_time,
                 nsteps
@@ -45,3 +49,41 @@ class EuropeanCall(Option):
 
         payoffs = np.array([self.payoff_func(price[0]) for price in x])
         return payoffs
+
+    def vanilla_montecarlo(self, target_stdev, confidence_level):
+        '''
+        Args:
+            target_stdev(float): target stdev of the estimator of the option price
+                then epsilon will be like 1.98 * target_stdev
+            confidence_level(float): between [0.5, 1] e.g. 0.9 for 90% confidence
+        '''
+        T = self.expirary_time
+        r = self.risk_free
+
+        z_score = norm.ppf(1 - (1 - confidence_level) / 2)
+        target_epsilon = z_score * target_stdev
+
+        npaths = int(target_epsilon**(-2))
+        step_size = target_epsilon
+        nsteps = int(math.floor(T / step_size))
+
+        simulated_payoffs = self.euler_asset_walk(nsteps, npaths)
+        return np.mean(simulated_payoffs) * math.exp(-r*T)
+
+    def check_vanilla_mc(self, target_stdev, confidence_level, num_samples=1000):
+        samples_option_values = [self.vanilla_montecarlo(target_stdev, confidence_level) for i in xrange(num_samples)]
+
+        S0 = self.assets.spot
+        K = self.strike
+        r = self.risk_free
+        q = 0
+        sigma = self.assets._vol
+        T = self.expirary_time
+        option_type = 'call'
+        true_value = black_scholes(S0, K, r, q, sigma, T, option_type)
+
+        z_score = norm.ppf(1 - (1 - confidence_level) / 2)
+        target_epsilon = z_score * target_stdev
+        checks = [abs(x - true_value) > target_epsilon for x in samples_option_values]
+        print(checks)
+        return (sum(checks) / len(checks)) <= (1 - confidence_level), sum(checks), len(checks)
